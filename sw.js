285338502d9117ccecdce024b7a11552c8f66e4d
Darky <179386711+Darky-Github@users.noreply.github.com>
2025-06-16 18:09:51 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
+self.addEventListener('install', e => {
+  console.log('[SW] Installed');
+  e.waitUntil(
+    caches.open('jsdroid-cache').then(cache =>
+      cache.addAll(['/', '/index.html', '/css/main.css', '/js/core.js'])
+    )
+  );
+});
 
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
